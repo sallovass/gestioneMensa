b6f306706b98ba129934241ac1bfd2bc4bc0f7f0
Forzati Patrick <92048193+patrickforzati@users.noreply.github.com>
2022-02-21 16:06:57 +0100

    Add files via upload

---
+++ mensa_agrario/public/recuperoPassword.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Recupero password</title>
+</head>
+<body>
+	<h1>Recupero password</h1>
+	<form method = "get">
+		Email <input type = "email" name = "email"/> </br>
+		<button type = "submit">Richiedi reset</button>
+	</form>
+</body>
+</html>
